2d6927e1ab73f4bbae8488272f839c24f2d95413
bertillesmith <bertilled.smith@gmail.com>
2021-06-03 15:19:41 -0400

    added AICc to cell_density script and created a document in src " AICc_calculation_script.R" can be called at the beginning

---
+++ AICc_calculation_script.R
@@ -0,0 +1,26 @@
+
+#calculate AIC values for all models
+aic_func <- function(npar, nsamp, rss){
+  aic_val <- 2*npar + nsamp*log(rss/nsamp)
+  return(aic_val)
+}  
+
+aicc_func <- function(npar, nsamp, aic_val){
+  if (nsamp <= npar+1){
+    aicc_val <- NA
+    return(aicc_val)
+  } else{
+    aicc_val <- aic_val + ((2*npar^2) + 2*npar)/(nsamp - npar - 1)
+  }
+  return(aicc_val)
+}
+
+delta_aicc_func <- function(min_aicc, aicc_val){
+  delta_aicc <- aicc_val - min_aicc
+  return(delta_aicc)
+}
+
+rmse_func <- function(obs, pred){
+  rmse_val <- sqrt(mean((obs-pred)^2))
+  return(rmse_val)
+}
